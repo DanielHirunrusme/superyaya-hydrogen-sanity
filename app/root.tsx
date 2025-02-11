7fd9f98ff106f3d286c69e9a297b4c88c9195815
Daniel Hirunrusme <daniel@halfhalf.us>
2025-02-11 12:36:28 -0600

    test cookie policy

--- app/root.tsx
+++ app/root.tsx
@@ -15,34 +15,34 @@ import {
   type SeoHandleFunction,
   ShopifySalesChannel,
 } from '@shopify/hydrogen';
-import type {Collection, Shop} from '@shopify/hydrogen/storefront-api-types';
+import type { Collection, Shop } from '@shopify/hydrogen/storefront-api-types';
 import {
   defer,
   type LinksFunction,
   type LoaderArgs,
 } from '@shopify/remix-oxygen';
-import {getPreview, PreviewProvider} from 'hydrogen-sanity';
+import { getPreview, PreviewProvider } from 'hydrogen-sanity';
 
-import {GenericError} from '~/components/global/GenericError';
-import {Layout} from '~/components/global/Layout';
-import {NotFound} from '~/components/global/NotFound';
-import {PreviewLoading} from '~/components/global/PreviewLoading';
-import {useAnalytics} from '~/hooks/useAnalytics';
-import {useNonce} from '~/lib/nonce';
-import {DEFAULT_LOCALE} from '~/lib/utils';
-import {LAYOUT_QUERY} from '~/queries/sanity/layout';
-import {COLLECTION_QUERY_ID} from '~/queries/shopify/collection';
+import { GenericError } from '~/components/global/GenericError';
+import { Layout } from '~/components/global/Layout';
+import { NotFound } from '~/components/global/NotFound';
+import { PreviewLoading } from '~/components/global/PreviewLoading';
+import { useAnalytics } from '~/hooks/useAnalytics';
+import { useNonce } from '~/lib/nonce';
+import { DEFAULT_LOCALE } from '~/lib/utils';
+import { LAYOUT_QUERY } from '~/queries/sanity/layout';
+import { COLLECTION_QUERY_ID } from '~/queries/shopify/collection';
 import stylesheet from '~/styles/tailwind.css';
-import type {I18nLocale} from '~/types/shopify';
+import type { I18nLocale } from '~/types/shopify';
 import IntroWrapper from './components/global/IntroWrapper';
-import {ThemeProvider, useTheme} from './components/context/ThemeProvider';
+import { ThemeProvider, useTheme } from './components/context/ThemeProvider';
 import clsx from 'clsx';
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
-const seo: SeoHandleFunction<typeof loader> = ({data}) => ({
+const seo: SeoHandleFunction<typeof loader> = ({ data }) => ({
   title: data?.layout?.seo?.title,
-  titleTemplate: `%s${
-    data?.layout?.seo?.title ? ` · ${data?.layout?.seo?.title}` : ''
-  }`,
+  titleTemplate: `%s${data?.layout?.seo?.title ? ` · ${data?.layout?.seo?.title}` : ''
+    }`,
   description: data?.layout?.seo?.description,
 });
 
@@ -52,7 +52,7 @@ export const handle = {
 
 export const links: LinksFunction = () => {
   return [
-    {rel: 'stylesheet', href: stylesheet},
+    { rel: 'stylesheet', href: stylesheet },
     {
       href: 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,500;0,700;1,500;1,700&display=swap',
       rel: 'stylesheet',
@@ -78,8 +78,8 @@ export const links: LinksFunction = () => {
   ];
 };
 
-export async function loader({context}: LoaderArgs) {
-  const {cart} = context;
+export async function loader({ context }: LoaderArgs) {
+  const { cart } = context;
 
   const cache = context.storefront.CacheCustom({
     mode: 'public',
@@ -90,8 +90,8 @@ export async function loader({context}: LoaderArgs) {
   const preview = getPreview(context);
 
   const [shop, layout] = await Promise.all([
-    context.storefront.query<{shop: Shop}>(SHOP_QUERY),
-    context.sanity.query<any>({query: LAYOUT_QUERY, cache}),
+    context.storefront.query<{ shop: Shop }>(SHOP_QUERY),
+    context.sanity.query<any>({ query: LAYOUT_QUERY, cache }),
   ]);
 
   const selectedLocale = context.storefront.i18n as I18nLocale;
@@ -105,15 +105,15 @@ export async function loader({context}: LoaderArgs) {
     cart: cart.get(),
     layout,
     notFoundCollection: layout?.notFoundPage?.collectionGid
-      ? context.storefront.query<{collection: Collection}>(
-          COLLECTION_QUERY_ID,
-          {
-            variables: {
-              id: layout.notFoundPage.collectionGid,
-              count: 16,
-            },
+      ? context.storefront.query<{ collection: Collection }>(
+        COLLECTION_QUERY_ID,
+        {
+          variables: {
+            id: layout.notFoundPage.collectionGid,
+            count: 16,
           },
-        )
+        },
+      )
       : undefined,
     sanityProjectID: context.env.SANITY_PROJECT_ID,
     sanityDataset: context.env.SANITY_DATASET,
@@ -123,11 +123,11 @@ export async function loader({context}: LoaderArgs) {
 }
 
 function App() {
-  const {preview, ...data} = useLoaderData<typeof loader>();
+  const { preview, ...data } = useLoaderData<typeof loader>();
   const locale = data.selectedLocale ?? DEFAULT_LOCALE;
   const hasUserConsent = true;
   const nonce = useNonce();
-  const {theme, navVisible} = useTheme();
+  const { theme, navVisible } = useTheme();
 
   useAnalytics(hasUserConsent, locale);
 
@@ -152,7 +152,7 @@ function App() {
           as="font"
           type="font/woff2"
           crossOrigin="anonymous"
-          
+
         />
         <link
           rel="preload"
@@ -206,6 +206,20 @@ function App() {
           type="font/woff"
           crossOrigin="anonymous"
         />
+        <script type="text/javascript">
+          {`
+            var _iub = _iub || [];
+            _iub.csConfiguration = {
+              "siteId": 3926637,
+              "cookiePolicyId": 33461147,
+              "lang": "en",
+              "storage": {"useSiteId": true}
+            };
+          `}
+        </script>
+        <script type="text/javascript" src="https://cs.iubenda.com/autoblocking/3926637.js"></script>
+        <script type="text/javascript" src="//cdn.iubenda.com/cs/gpp/stub.js"></script>
+        <script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" async></script>
       </head>
       <body>
         <PreviewProvider previewConfig={preview} fallback={<PreviewLoading />}>
@@ -232,7 +246,7 @@ export default function AppWithProviders() {
   );
 }
 
-export function ErrorBoundary({error}: {error: Error}) {
+export function ErrorBoundary({ error }: { error: Error }) {
   const [root] = useMatches();
   const nonce = useNonce();
 
@@ -244,9 +258,9 @@ export function ErrorBoundary({error}: {error: Error}) {
     layout,
     notFoundCollection,
   } = root.data
-    ? root.data
-    : {selectedLocale: DEFAULT_LOCALE, layout: null, notFoundCollection: {}};
-  const {notFoundPage} = layout || {};
+      ? root.data
+      : { selectedLocale: DEFAULT_LOCALE, layout: null, notFoundCollection: {} };
+  const { notFoundPage } = layout || {};
 
   let title = 'Error';
   if (isRouteError) {
@@ -276,7 +290,7 @@ export function ErrorBoundary({error}: {error: Error}) {
                 />
               ) : (
                 <GenericError
-                  error={{message: `${routeError.status} ${routeError.data}`}}
+                  error={{ message: `${routeError.status} ${routeError.data}` }}
                 />
               )}
             </>
